080fc0911304aa7fc336c882fd8b59b61214d297
bgebes <berkay.gebes@bil.omu.edu.tr>
2023-02-02 19:00:31 +0300

    feat: added @apollo/client and graphql packages

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './apollo/client';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
+  <ApolloProvider client={client}>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </ApolloProvider>
 );
